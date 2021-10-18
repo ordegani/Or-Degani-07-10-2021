2022d46c37ab00dd15fc6510761f724e3050fc94
ordegani <oradeganii@gmail.com>
2021-10-18 15:31:30 +0300

    changes

--- src/Saved.jsx
+++ src/Saved.jsx
@@ -2,30 +2,12 @@ import { React } from "react";
 import Result from "./Result";
 
 export default function Saved({ save, setsave }) {
-  // useEffect(() => {
-  //   const fetch = async () => {
-  //     const data = await axios.get("http://localhost:5000/");
-  //     console.log(data);
-  //   };
-  //   fetch();
-  // }, []);
-
   save.forEach((element) => {
     console.log(element);
   });
   const remove = (element) => {
     setsave(save.filter((save) => save.GeoPosition !== element.GeoPosition));
   };
-
-  // useEffect(() => {
-  //   const postSave = () => {
-  //     axios.post("/api/users/login", save).then((res) => console.log(res));
-  //     console.log(save);
-  //   };
-  //   return () => {
-  //     postSave();
-  //   };
-  // }, [save]);
 
   return (
     <div className="homeContainer">
@@ -39,9 +21,9 @@ export default function Saved({ save, setsave }) {
       <div className="savedMoviesContainer">
         {save.map((save) => (
           <Result
-            LocalizedName={location[2].LocalizedName}
-            Latitude={location[4].GeoPosition.Latitude}
-            Longitude={location[4].GeoPosition.Longitude}
+            LocalizedName={save[2].LocalizedName}
+            Latitude={save[4].GeoPosition.Latitude}
+            Longitude={save[4].GeoPosition.Longitude}
             onClick={remove}
             text="Delete"
           />
